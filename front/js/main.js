5170dbdba4907a18285931e29b8fc3ab1dbcd6f7
kulakovskyi <torchok9@gmail.com>
2023-02-21 16:47:24 +0200

    add desc 1920

--- front/js/main.js
+++ front/js/main.js
@@ -1,41 +1,51 @@
-(function () {
-    var url = new URL(window.location.href);
-    var params = ['l', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content', 'param1', 'param2'];
-    var linkParams = ['affid', 'cpaid']; // ищем в url redirectUrl в url:
-
-    if (url.searchParams.has('redirectUrl')) {
-        var redirectUrl = new URL(url.searchParams.get('redirectUrl'));
-
-        if (redirectUrl.href.match(/\//g).length === 4 && redirectUrl.searchParams.get('l')) {
-            //если ссылка в ссылка redirectUrl корректная
-            localStorage.setItem('redirectUrl', redirectUrl.href); // указываем точкой входа домен с протоколом из redirectUrl
-        }
-    } /////////
+let body = document.querySelector('body')
+let landStart = document.querySelector('.land__start');
+let landContent = document.querySelector('.land__content');
+let slides = document.querySelectorAll('.slide');
 
 
-    params.forEach(function (param) {
-        if (url.searchParams.has(param)) localStorage.setItem(param, url.searchParams.get(param));
-    });
-    linkParams.forEach(function (linkParam) {
-        if (url.searchParams.has(linkParam)) localStorage.setItem(linkParam, url.searchParams.get(linkParam));
-    });
-    window.addEventListener('click', function (e) {
-        var link,
-            parent = e.target.closest('a');
+window.setTimeout(hiddenStart, 3000);
 
-        if (parent.getAttribute('href') !== 'https://tds.favbet.partners') {
-            return;
-        }
+function hiddenStart(){
+    landStart.classList.add('_hidden')
+    landContent.classList.remove('_hidden')
+    body.classList.remove('_lock')
 
-        parent && (e.preventDefault(),
-            localStorage.getItem("redirectUrl")
-                ? link = new URL(localStorage.getItem("redirectUrl"))
-                : (link = new URL(parent.href),
-                    affid = localStorage.getItem('affid'),
-                    cpaid = localStorage.getItem('cpaid'),
-                affid && cpaid && (link.pathname = '/' + affid + '/' + cpaid)), params.forEach(function (param)
-        {
-            url.searchParams.has(param) && link.searchParams.set(param, localStorage.getItem(param));
-        }), document.location.href = link);
-    });
-})();
+    let currentSlide = 0;
+    let slideInterval = setInterval(nextSlide,3500);
+    function nextSlide(){
+        goToSlide(currentSlide+1);
+    }
+
+    function previousSlide(){
+        goToSlide(currentSlide-1);
+    }
+
+    function goToSlide(n){
+        slides[currentSlide].className = 'slide';
+        currentSlide = (n+slides.length)%slides.length;
+        slides[currentSlide].className = 'slide showing';
+    }
+
+}
+
+const copyCodeContent = 'FUN100';
+const copyBtn = document.querySelector('.land__content-bonus-copy');
+const codeCopy = document.querySelector('.land__content-bonus-copy-text');
+
+copyBtn.addEventListener('click', ()=>{
+    navigator.clipboard.writeText(copyCodeContent)
+        .then(() => {
+            console.log('Text copied to clipboard');
+            codeCopy.classList.add('copy')
+            copyBtn.disabled = true;
+            setTimeout(()=>{
+                codeCopy.classList.remove('copy');
+                copyBtn.disabled = false;
+            }, 5000)
+        })
+})
+
+
+
+
